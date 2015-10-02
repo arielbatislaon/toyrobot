609571fa9e47bd125d1490f5afb3690c63a0ede9
arielbatislaon <ariel.batislaon@gmail.com>
2015-10-02 17:46:50 +1000

    test spec for robot command
    test spec for robot command

--- spec/toyrobot_spec.rb
+++ spec/toyrobot_spec.rb
@@ -15,30 +15,60 @@ RSpec.describe ToyRobot do
 
   describe "ToyRobot features" do
     it "is placeable" do
-     @robot.place.should_not raise_error  
+     @robot.should respond_to(:place)  
     end
   
   
     it "is moveable" do
-      @robot.move.should_not raise_error  
+      @robot.should respond_to(:move)  
     end
   
 
   
     it "is moveable to the left" do
-      @robot.left.should_not raise_error  
+      @robot.should respond_to(:left)  
     end
   
 
   
     it "is moveable to the right" do
-      @robot.right.should_not raise_error 
+      @robot.should respond_to(:right)  
     end
   
 
   
     it "can report current position" do
-      @robot.report.should_not raise_error
+      @robot.should respond_to(:report)  
     end
- end 
+    
+    it "can accept command" do
+      @robot.should respond_to(:doCommand)  
+    end
+    
+  end
+  
+  describe "doComand to only accept valid comands: [PLACE,MOVE,LEFT,RIGHT,REPORT]  " do
+    it "calls robot.place if command=PLACE" do
+      @robot.should_receive(:place).with("PLACE")
+      @robot.doCommand("PLACE")
+    end
+    it "calls robot.move if command=MOVE" do
+      @robot.should_receive(:move).with("MOVE")
+      @robot.doCommand("MOVE")
+    end
+    it "calls robot.left if command=LEFT" do
+      @robot.should_receive(:left).with("LEFT")
+      @robot.doCommand("LEFT")
+    end
+    it "calls robot.right if command=RIGHT" do
+      @robot.should_receive(:right).with("RIGHT")
+      @robot.doCommand("RIGHT")
+    end
+    it "calls robot.report if command=REPORT" do
+      @robot.should_receive(:report).with("REPORT")
+      @robot.doCommand("REPORT")
+    end
+    
+  end  
+  
 end 
