8a334def512c3cd2f1d76c4d699d2b107e98b634
arielbatislaon <ariel.batislaon@gmail.com>
2015-10-02 14:55:01 +1000

    Put before each hook for each spec example

--- spec/toyrobot_spec.rb
+++ spec/toyrobot_spec.rb
@@ -2,20 +2,43 @@
 
 require 'rspec/autorun'
 
+require "rspec/expectations"
+
 require_relative '../classes/toyrobot'
 
 RSpec.describe ToyRobot do
-  describe "#place" do
-    it "is placeable" do
-      robot = ToyRobot.new
-      robot.place.should_not raise_error  raise_error
-    end
+  
+  before(:each) do
+    @robot = ToyRobot.new
   end
 
-  describe "#move" do
-    it "is moveable" do
-      robot = ToyRobot.new
-      robot.move.should_not raise_error  raise_error
+
+  describe "ToyRobot features" do
+    it "is placeable" do
+     @robot.place.should_not raise_error  raise_error
     end
-  end  
-end
+  
+  
+    it "is moveable" do
+      @robot.move.should_not raise_error  raise_error
+    end
+  
+
+  
+    it "is moveable to the left" do
+      @robot.left.should_not raise_error  raise_error
+    end
+  
+
+  
+    it "is moveable to the right" do
+      @robot.right.should_not raise_error  raise_error
+    end
+  
+
+  
+    it "can report current position" do
+      @robot.position.should_not raise_error  raise_error
+    end
+ end 
+end 
